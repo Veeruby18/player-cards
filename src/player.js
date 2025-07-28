19b1ce039c9c825e8d99ef6622c6ed24014463c3
Veeruby18 <odueloghen@gmail.com>
2025-07-28 11:37:12 +0100

    Create player.js

---
+++ src/player.js
@@ -0,0 +1,34 @@
+export const players = [
+  {
+    name: 'Lionel Messi',
+    team: 'Paris Saint-Germain',
+    nationality: 'Argentina',
+    jerseyNumber: 30,
+    age: 34,
+    image: 'https://via.placeholder.com/150'
+  },
+  {
+    name: 'Cristiano Ronaldo',
+    team: 'Manchester United',
+    nationality: 'Portugal',
+    jerseyNumber: 7,
+    age: 36,
+    image: 'https://via.placeholder.com/150'
+  },
+  {
+    name: 'Kylian Mbappé',
+    team: 'Paris Saint-Germain',
+    nationality: 'France',
+    jerseyNumber: 7,
+    age: 23,
+    image: 'https://via.placeholder.com/150'
+  },
+  {
+    name: 'Neymar Jr.',
+    team: 'Paris Saint-Germain',
+    nationality: 'Brazil',
+    jerseyNumber: 10,
+    age: 29,
+    image: 'https://via.placeholder.com/150'
+  }
+];
